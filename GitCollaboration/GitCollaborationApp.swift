56a715e08cc8e6a3480b3231d3f7de6a5d00a461
Amatullah <l.amatullah00@gmail.com>
2025-07-24 16:24:52 +0100

    Initial Commit

---
+++ GitCollaboration/GitCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitCollaborationApp.swift
+//  GitCollaboration
+//
+//  Created by Amatullah Lonandwala on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct GitCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
